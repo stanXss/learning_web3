dbc294a97a65c72e1c60c357d1c9505b0d74a447
stanXss <stanislav.salnikov@gmail.com>
2025-12-18 17:08:30 +0500

    Minor refactor, added comments to functions

--- simple_indexer/main.py
+++ simple_indexer/main.py
@@ -27,19 +27,30 @@ ERC20_TRANSFER_ABI = json.loads("""
 ]
 """)
 
-TOKEN = Web3.to_checksum_address("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
-DECIMALS = 6
+TOKEN = Web3.to_checksum_address("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")  # USDC address
+DECIMALS = 6  # decimals for USDC
 TRANSFER_TOPIC = Web3.keccak(text="Transfer(address,address,uint256)").hex()
-w3 = Web3(Web3.HTTPProvider(RPC_URL))
+w3 = Web3(Web3.HTTPProvider(RPC_URL))  # web3 RPC service
+conn = psycopg2.connect(CONN_LINE)  # init db connection
 
 
 def proc_main():
 
+    last_block = get_max_loaded_block()
+
     current_block = w3.eth.block_number
+
+    if last_block is None:  # only take several blocks because of rpc limit
+        last_block = current_block - 5
+    else:
+        if current_block - last_block > 5:
+            last_block = current_block - 5
+
+    print(last_block)
     print(current_block)
 
     logs = w3.eth.get_logs({
-        "fromBlock": current_block - 1,
+        "fromBlock": last_block,
         "toBlock":   current_block,
         "address":   TOKEN,
         "topics":    [TRANSFER_TOPIC]
@@ -51,23 +62,18 @@ def proc_main():
     logs_out = []
     for log in logs:
         log_out = decode_transfer_log(log, blk_times)
-        logs_out.append(make_row(log_out, decimals=6))
+        logs_out.append(make_row(log_out, decimals=DECIMALS))
 
     print(blk_times)
 
-    conn = psycopg2.connect(CONN_LINE)
+    bulk_insert_transfers(logs_out)
+
+    load_block_numbers(blk_times)
+
+
+def load_block_numbers(blk_times): # load block data into db
+
     cur = conn.cursor()
-
-    SQL_INSERT = """
-    INSERT INTO eth_main.erc20_transfers (
-      blockchain, token_address, tx_hash, log_index,
-      block_number, block_time, from_address, to_address,
-      amount_raw, amount_decimals
-    ) VALUES %s
-    ON CONFLICT (block_number, tx_hash, log_index) DO NOTHING;
-    """
-
-    bulk_insert_transfers(conn, logs_out, SQL_INSERT)
 
     for key, value in blk_times.items():
         cur.execute(
@@ -86,21 +92,33 @@ def proc_main():
     conn.commit()
 
 
-def bulk_insert_transfers(conn, rows, sql, page_size=5000):
+def bulk_insert_transfers(rows, page_size=5000):  # load txs into db
+    cur = conn.cursor()
+
     """
     rows: list of tuples in the exact column order above.
     Use page_size 1k–10k. Start with 5k; tune later.
     """
+
+    SQL_INSERT = """
+    INSERT INTO eth_main.erc20_transfers (
+      blockchain, token_address, tx_hash, log_index,
+      block_number, block_time, from_address, to_address,
+      amount_raw, amount_decimals
+    ) VALUES %s
+    ON CONFLICT (block_number, tx_hash, log_index) DO NOTHING;
+    """
+
     if not rows:
         return 0
 
     with conn.cursor() as cur:
-        execute_values(cur, sql, rows, page_size=page_size)
+        execute_values(cur, SQL_INSERT, rows, page_size=page_size)
     conn.commit()
     return len(rows)
 
 
-def get_block_timestamps_bulk(block_numbers, batch_size=100):
+def get_block_timestamps_bulk(block_numbers, batch_size=100):  # get block timestamps for blocks
     result = {}
 
     for i in range(0, len(block_numbers), batch_size):
@@ -130,7 +148,21 @@ def get_block_timestamps_bulk(block_numbers, batch_size=100):
         return out
 
 
-def make_row(decoded, decimals: int) -> tuple:
+def get_max_loaded_block():  # get max block number already loaded into db
+    SQL_GET_MAX_BLOCK = """
+    select
+    max(block_number) block
+    from eth_main.block_cache bc
+    """
+
+    cur = conn.cursor()
+
+    cur.execute(SQL_GET_MAX_BLOCK)
+    last_block = int(cur.fetchone()[0])
+    return last_block
+
+
+def make_row(decoded, decimals: int) -> tuple:  # prepare row for db insert
     # decoded: dict with from_address, to_address, amount_raw, tx_hash, log_index, block_number, block_time (unix int)
     return (
         "ethereum",
@@ -146,7 +178,7 @@ def make_row(decoded, decimals: int) -> tuple:
     )
 
 
-def decode_transfer_log(log, blk_times):
+def decode_transfer_log(log, blk_times):  # decode logs with ABI
 
     token_contract = w3.eth.contract(address=TOKEN, abi=ERC20_TRANSFER_ABI)
 
@@ -165,7 +197,7 @@ def decode_transfer_log(log, blk_times):
     }
 
 
-def hex_to_bytes20(addr: str) -> bytes:
+def hex_to_bytes20(addr: str) -> bytes:  # hex to bytes for addresses
     # expects '0x' + 40 hex
     a = addr.lower()
     if a.startswith("0x"):
@@ -175,7 +207,7 @@ def hex_to_bytes20(addr: str) -> bytes:
     return bytes.fromhex(a)
 
 
-def hex_to_bytes32(h: str) -> bytes:
+def hex_to_bytes32(h: str) -> bytes:  # hex to bytes for tx hashes
     # expects '0x' + 64 hex, or sometimes without 0x
     x = h.lower()
     if x.startswith("0x"):
@@ -185,10 +217,11 @@ def hex_to_bytes32(h: str) -> bytes:
     return bytes.fromhex(x)
 
 
-def parse_block_number(x):
+def parse_block_number(x):  # block number from hex to int if needed
     if isinstance(x, int):
         return x
     return int(x, 16)
+
 
 if __name__ == '__main__':
     proc_main()
